ff4a762c833d95a5d5ddb3dc36022bedfad0fac5
Anugrah Ramadhan <dialog.anugrah@gmail.com>
2019-09-04 00:00:56 +0700

    1st

---
+++ 1. memahami struktur program/UjangSutisna.java
@@ -0,0 +1,24 @@
+//kagak pake visibility bisa loh
+class UjangSutisna {
+
+// harus ada isinya nih. biar bisa di-run
+// karena Java Virtual Machine bakal nyari pintu programmnya dari mana? nah pintu itu teh namanya main()
+
+//nah kayak gini isinya
+
+    public static void main(String[] args){
+        // nah ini body lagi, karena di dalem kurung kurawal juga. body dalam body laaaah
+        System.out.print("anjing lu bangsat");
+
+        // System teh library java yang dipakai untuk ngakses console apapun nanti yang dipakai. CMD lah contohnya
+        // out teh buat ngirim data ke console tea
+        // print() --> itu teh perintahnya. untuk nyetak sesuatu itu teh. echo lah kalau di php mah
+        // jangan lupa titik koma bos
+    }
+
+// public di atas itu juga visibility. Pastinya ada private juga. Sama lah kayak php
+
+// main() --> udah default nih. harus ada di program java. pintunya tea geningan.
+
+
+}
