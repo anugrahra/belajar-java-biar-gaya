0720cef059fd5eb42200fbf14db6f9f121552e14
Anugrah Ramadhan <dialog.anugrah@gmail.com>
2019-09-04 00:44:36 +0700

    IDE

---
+++ 2. Organisasi Program dengan Package/program dengan metode manual/src/com/dekadensiotak/Main.java
@@ -0,0 +1,16 @@
+package com.dekadensiotak;
+
+public class Main{
+    
+    // ini teh disebut juga method
+    public static void main(String[] args){
+        System.out.println("hello mantan");
+    }
+}
+
+// {} --> Body-nya
+// Class --> harus ada, keywords bersama Public
+// Public --> Visibility, keywords bersama Class
+// Main--> identifier dari class-nya. Penamaannya harus sama dengan nama file-nya. Huruf depannya harus besar. Kalau namanya panjang begimana? Pake camel case. Onta style
+
+//dicompile di cmd pake javac
