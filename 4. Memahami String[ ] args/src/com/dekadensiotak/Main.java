d9f3334e2635f6ed81ffc0dad4f2afd77b41ebc3
Anugrah Ramadhan <dialog.anugrah@gmail.com>
2019-09-05 00:03:19 +0700

    variabel, assignment, deklarasi

---
+++ 4. Memahami String[ ] args/src/com/dekadensiotak/Main.java
@@ -0,0 +1,12 @@
+package com.dekadensiotak;
+
+public class Main {
+
+    // string[] args adalah input yang bisa dimasukin saat mengeksekusi java
+    // [] --> array / kumpulan data tea gening
+    // masukin datanya di console bos!
+    public static void main(String[] args){
+        System.out.println("Hei " + args[0] + "!");
+        System.out.println("Bangsat kau " + args[1] + "!");
+    }
+}
---
+++ 5. Variabel, Assignment, dan Deklarasi/src/com/dekadensiotak/Main.java
@@ -0,0 +1,28 @@
+package com.dekadensiotak;
+
+public class Main {
+
+    public static void main(String[] args){
+        // VARIABLE
+        // nilainya bisa berubah
+        int x = 10; // ini teh assignment
+
+        // int adalah tipe data
+        // x adalah nama variable / identifier
+            // namanya teh gaboleh yang udah ada di java
+        // = adalah operator assignment
+        // 10 adalah nilai variable
+        // nilainya ditaruh di memori komputer
+
+        System.out.println("nilai x = " + x);
+
+        x = 20;
+        System.out.println("nilai x baru = " + x);
+
+        // DEKLARASI
+        int b; // nah gini nih, cuman ngasih tau doang kalau b teh adalah integer
+
+        b = 7;
+        System.out.println("nilai b = " + b);
+    }
+}
