8c09df0d75bb1640f1aecad0484c3393df0065e5
Anugrah <dialog.anugrah@gmail.com>
2019-09-16 00:07:07 +0700

    Update UjangSutisna.java

--- 1. memahami struktur program/UjangSutisna.java
+++ 1. memahami struktur program/UjangSutisna.java
@@ -1,13 +1,26 @@
 //kagak pake visibility bisa loh
 class UjangSutisna {
-
+// class adalah container untuk fungsi-fungsi yang berhubungan
+    
 // harus ada isinya nih. biar bisa di-run
 // karena Java Virtual Machine bakal nyari pintu programmnya dari mana? nah pintu itu teh namanya main()
+// jadi, setiap class di java harus ada main function
 
 //nah kayak gini isinya
-
-    public static void main(String[] args){
+    
+    public static void main(String[] args) {
         // nah ini body lagi, karena di dalem kurung kurawal juga. body dalam body laaaah
+
+        // ini teh function
+        // function adalah suatu blok kode untuk melakukan suatu perintah
+        // dalam java, minimal harus ada satu fungsi
+
+        // void adalah salah satu return type
+        // main() -> the door tea gening -- adalah nama fungsinya
+        
+        // ini juga adalah method
+        // method adalah fungsi yang merupakan bagian dari sebuah class
+    
         System.out.print("anjing lu bangsat");
 
         // System teh library java yang dipakai untuk ngakses console apapun nanti yang dipakai. CMD lah contohnya
