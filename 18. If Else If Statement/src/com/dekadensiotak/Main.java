753c114a61389b50570748a0e594455e6e374fdc
Anugrah Ramadhan <dialog.anugrah@gmail.com>
2019-09-22 08:09:22 +0700

    if else if

---
+++ 18. If Else If Statement/src/com/dekadensiotak/Main.java
@@ -0,0 +1,23 @@
+package com.dekadensiotak;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a = 9;
+        System.out.println("ini adalah awal program");
+        // if else if statement
+
+        if (a == 5){
+            System.out.println("ini adalah aksi satu");
+        } else if (a == 10) {
+            System.out.println("ini adalah aksi dua");
+        } else {
+            System.out.println("ini adalah aksi default");
+        }
+
+
+        // akhir dari if else if statement
+        System.out.println("ini adalah akhir program");
+    }
+}
